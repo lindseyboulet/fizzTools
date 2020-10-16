ec34d4d102d94fd7e07194afcb4fc7cc1878f0cd
lindseyboulet <lindseyboulet>
2020-10-16 14:07:22 -0700

    silo_select_load commit

---
+++ R/silo_select_load.R
@@ -0,0 +1,35 @@
+#' silo_select_load {fizzTulz}
+#' 
+#' @title Combine and Label Silo Selected Output Files  
+#'
+#' @description Combines files from silo > output > selectData into a single data frame with labels
+#' 
+#' @param file.dir full 'selectData' directory path
+#'
+#' @examples
+#' silo_select_load('./silo/output/selectData/')
+#' 
+#'
+#' @export
+
+silo_select_load <-
+  function(file.dir){
+    fN <- list.files(file.dir, full.names = TRUE)
+    nM <- list.files(file.dir)
+    x <- lapply(fN, read.csv)
+    nM <- strtrim(nM, nchar(nM)-4)
+    names(x) <- nM
+    nM2 <- as.data.frame(matrix(as.character(unlist(strsplit(nM, "_"))), 
+                                ncol = sapply(gregexpr("_", nM[1]), length) +1, byrow = TRUE))
+    nM2 <- cbind(nM2, nM)
+    drop.ind <- grep('sec', nM2)[1]
+    nM2 <- nM2[,-c(drop.ind:(drop.ind+2))]
+    for(i in 1:length(x)){
+      df <- x[[i]]
+      df$id <- nM2[which(names(x)[i]==nM2[,3]),1]
+      df$cond <- nM2[which(names(x)[i]==nM2[,3]),2]
+      x[[i]] <- df
+    }
+    x
+  }
+
